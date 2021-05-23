ae623fa2421bc07df6461b56aa0df544bdc39ffd
melquiadesmario <melquiadesm@gmail.com>
2021-05-23 12:37:43 -0300

    Create Entity Interface and Repository Cities

---
+++ src/main/java/com/github/melquiadesmario/citiesapi/cities/CityRepository.java
@@ -0,0 +1,6 @@
+package com.github.melquiadesmario.citiesapi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
